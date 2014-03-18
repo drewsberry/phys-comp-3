c9afedb3a8c832db7bb79bcdc6bc6cfedf7b0c2d
Drewsberry <as1423@my.bristol.ac.uk>
2014-03-18 22:16:17 +0000

    Uncomment temp progress bar file

--- random/temp/temp.py
+++ random/temp/temp.py
@@ -1,17 +1,17 @@
 ## My progress bar - maybe I'll need it later
 
-# even = np.zeros((num))
+even = np.zeros((num))
 
-# print "Random numbers produced: ",
+print "Random numbers produced: ",
 
-# for i in range(num):
+for i in range(num):
 
-#     even[i] = rnd.uniform(0,2)
+    even[i] = rnd.uniform(0,2)
 
-#     percentage = 100*(i+1)/num
+    percentage = 100*(i+1)/num
 
-#     if percentage%1 == 0:
-#         sys.stdout.flush()
-#         sys.stdout.write("\r\t\t\t\t\t["+"#"*int(percentage)+" "*int(100 - percentage)+"]"+"(%3d%%)"%(percentage))
+    if percentage%1 == 0:
+        sys.stdout.flush()
+        sys.stdout.write("\r\t\t\t\t\t["+"#"*int(percentage)+" "*int(100 - percentage)+"]"+"(%3d%%)"%(percentage))
 
-# print
+print
