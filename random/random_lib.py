6c1f4a6a9091b577bb332b87f820d606a1db0cfb
drewsberry <silca6@hotmail.co.uk>
2014-03-19 14:04:33 +0000

    Finish reject-accept method; add fixnum optionl; parse args

--- random/random_lib.py
+++ random/random_lib.py
@@ -8,15 +8,15 @@ import math
 # External libraries
 import numpy as np
 
-rnd = random.SystemRandom()
-# Use system produced random numbers
+# rnd = random.SystemRandom()
+## Use system produced random numbers
 
 def produce_randoms(num):
     # Produce num evenly distributed random numbers 
 
+    print "Producing {} uniformly distributed random numbers... ".format(num),
     even = np.random.uniform(0,2,num)
-
-    print "Initial even random number distribution produced."
+    print "done"
 
     return even
 
@@ -26,26 +26,74 @@ def trans_to_sin(even):
 
     num = len(even)
 
+    print "Analytically transforming {} evenly distributed random numbers to "\
+          "sinusoidally distributed ones...".format(num),
     sinusoid = np.arccos(1 - even)
-
-    print "Sinusoidal random number distribution using analytic method produced."
+    print "done"
 
     return sinusoid
 
-def reject_accept(num):
+def reject_accept(num, verb):
     # Use reject-accept method to produce random numbers distributed as 
     # sin(theta) in 0 < theta < pi
 
-    first = np.random.uniform(0,math.pi,num)
-    second = np.random.uniform(0,1,num)
+    first = np.random.uniform(0,math.pi,5*num)
+    second = np.random.uniform(0,1,5*num)
+    sinusoid = []
 
     criterion = second < np.sin(first)
 
-    sinusoid = filter(lambda i: criterion[i], first)
+    print "Producing random sinusoidally distributed numbers using reject-accept method...",
 
-    print "Sinusoidal random number distribution using reject-accept method produced."
+    if verb:
+        print
+        print "Random numbers produced:",
+
+    for i in range(num):
+        if criterion[i]:
+            sinusoid.append(first[i])
+        if verb:
+            display_progress(i+1,num)
+
+    print "done"
+
+    if verb:
+        print "{} random numbers produced.".format(len(sinusoid))
 
     return sinusoid
+
+def reject_accept_fixed(num, verb):
+    # Use reject-accept method to produce fixed number of random sinusoidally 
+    # distributed numbers
+
+    # The 5 is just because chance if I produce 9 times the number I need then
+    # chances are at that I'll get at least num out (specifically, 1*num produces
+    # about 0.6*num out, so 9*num is insufficient in ~1% of all runs). There is a 
+    # trade-off here in terms of efficiency and processing time.
+    first = np.random.uniform(0,math.pi,9*num)
+    second = np.random.uniform(0,1,9*num)
+    sinusoid = []
+
+    criterion = second < np.sin(first)
+
+    print "Producing fixed number {} of random sinusoidally distributed numbers "\
+          "via reject-accept method...".format(num),
+
+    if verb:
+        print
+        print "Random numbers produced: ",
+
+    for i in range(len(criterion)):
+        if criterion[i]:
+            sinusoid.append(first[i])
+            if len(sinusoid) >= num:
+                print "done"
+
+                return sinusoid
+        if verb:
+            display_progress(len(sinusoid),num)
+
+    return False
 
 def display_progress(current, target):
     # Displays progress bar showing how close current is to target
@@ -55,5 +103,7 @@ def display_progress(current, target):
     if percentage%1 == 0:
         sys.stdout.flush()
         sys.stdout.write("\r\t\t\t\t\t["+"#"*int(percentage)+" "*int(100 - percentage)+"]"+"(%3d%%)"%(percentage))
+        if percentage == 100:
+            print
 
     return 0
--- random/random_numbers.py
+++ random/random_numbers.py
@@ -2,6 +2,7 @@ from __future__ import division
 
 # In-built libraries
 import time
+import argparse
 
 # External libraries
 import numpy as np
@@ -11,34 +12,72 @@ import matplotlib.pyplot as plt
 import random_lib as rndlib
 import random_plot as rndplt
 
-num_numbers = int(1e6)
+parser = argparse.ArgumentParser(description="Producing random numbers distributed as sin(theta)")
+
+parser.add_argument("-v", "--verbose", action="store_true",
+                    help="Increase verbosity; print information including progress bars - slows"\
+                         " down program.")
+
+parser.add_argument("-p", "--plot", action="store_true",
+                    help="Plots resulting random distributions to file.")
+
+parser.add_argument("-o", "--output",
+                    help="Write resulting output random numbers to file.")
+
+parser.add_argument("-n", "--num", type=float, default=1e6,
+                    help="Number of initial random numbers to produce. This will equal the number"\
+                         " of sinusoidally distributed random numbers for the analytic method, but"\
+                         " not for the reject-accept method unless the '-f'/'--fixnum' flags are"\
+                         " also used.")
+
+parser.add_argument("-f", "--fixnum", action="store_true",
+                    help="Produce fixed number of random sinusoidally distributed numbers.")
+
+parser.add_argument("-t", "--timed", action="store_true",
+                    help="Time operations for performance comparison.")
+
+# Arguments go here, once I've finalised and decided what they are
+
+args = parser.parse_args()
+
+num = int(args.num) # This means scientific notation can be accepted as argument
 
 start = time.clock()
-
-even = rndlib.produce_randoms(num_numbers)
-
+even = rndlib.produce_randoms(num)
 end = time.clock()
-print "Time taken: ", end - start
+if args.timed:
+    print "Processor time taken: ", end - start
 print
 
 # Analytic method
 
 start = time.clock()
-
 analytic_sin = rndlib.trans_to_sin(even)
-
 end = time.clock()
-
-print "Time taken: ", end - start
+if args.timed:
+    print "Processor time taken: ", end - start
 print
 
 # Reject-accept method
 
-start = time.clock()
-reject_accept_sin = rndlib.reject_accept(num_numbers)
-end = time.clock()
-print "Time taken: ", end - start
-print
+if not args.fixnum:
+    start = time.clock()
+    reject_accept_sin = rndlib.reject_accept(num, args.verbose)
+    end = time.clock()
+    if args.timed:
+        print "Processor time taken: ", end - start
+    print
 
-rndplt.plot_sin_hist(reject_accept_sin, title="Reject-Accept Method", fname="reject_accept")
-rndplt.plot_sin_hist(analytic_sin, title="Analytic Method", fname="analytic")
+# Fixed reject-accept method
+
+if args.fixnum:
+    start = time.clock()
+    reject_accept_sin = rndlib.reject_accept_fixed(num, args.verbose)
+    end = time.clock()
+    if args.timed:
+        print "Processor time taken: ", end - start
+    print
+
+if args.plot:
+    rndplt.plot_sin_hist(analytic_sin, title="Analytic Method", fname="analytic")
+    rndplt.plot_sin_hist(reject_accept_sin, title="Reject-Accept Method", fname="reject_accept")
